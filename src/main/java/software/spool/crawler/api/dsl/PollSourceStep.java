dda899b17e57d6b87841ff425d4d358b58d9fa1d
javcastilla <javier.casno@gmail.com>
2026-02-28 23:59:09 +0000

    feat: sender name has now a default if not specified

--- src/main/java/software/spool/crawler/api/dsl/PollSourceStep.java
+++ src/main/java/software/spool/crawler/api/dsl/PollSourceStep.java
@@ -1,11 +1,17 @@
 package software.spool.crawler.api.dsl;
 
+import software.spool.crawler.api.ErrorRouter;
 import software.spool.crawler.api.ProcessorFormat;
+import software.spool.crawler.api.port.EventBus;
+import software.spool.crawler.api.port.InboxWriter;
 import software.spool.crawler.api.source.PollSource;
 import software.spool.crawler.api.strategy.CrawlerStrategy;
 import software.spool.crawler.internal.strategy.PollCrawlerStrategy;
 import software.spool.crawler.internal.utils.CrawlerPorts;
 import software.spool.crawler.internal.utils.factory.Transformer;
+
+import java.util.Optional;
+import java.util.UUID;
 
 public class PollSourceStep<R, T, O> {
     private final PollSource<R> source;
@@ -33,8 +39,36 @@ public class PollSourceStep<R, T, O> {
         return this;
     }
 
+    public PollSourceStep<R, T, O> bus(EventBus bus) {
+        this.ports = getPorts(bus, ports.inboxWriter(), ports.errorRouter());
+        return this;
+    }
+
+    public PollSourceStep<R, T, O> inbox(InboxWriter inbox) {
+        this.ports = getPorts(ports.bus(), inbox, ports.errorRouter());
+        return this;
+    }
+
+    public PollSourceStep<R, T, O> errorRouter(ErrorRouter errorRouter) {
+        this.ports = getPorts(ports.bus(), ports.inboxWriter(), errorRouter);
+        return this;
+    }
+
+    private CrawlerPorts getPorts(EventBus bus, InboxWriter inboxWriter, ErrorRouter errorRouter) {
+        return CrawlerPorts.builder()
+                .bus(bus)
+                .inbox(inboxWriter)
+                .errorRouter(errorRouter)
+                .build();
+    }
+
     public CrawlerStrategy create() {
-        return new PollCrawlerStrategy<>(source, transformer, ports, sender);
+        return new PollCrawlerStrategy<>(source, transformer, ports, getSenderOrDefault());
+    }
+
+    private String getSenderOrDefault() {
+        return Optional.ofNullable(sender).orElseGet(() ->
+                "PollCrawler" + "-" + UUID.randomUUID().toString().substring(0, 8));
     }
 
     public <NT, NO> PollSourceStep<R, NT, NO> withFormat(ProcessorFormat<R, NT, NO> format) {
